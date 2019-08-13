b1cf33b8a3f01e3766179f547b3157e9f0399f57
wMiu <miu0619@163.com>
2019-08-13 10:34:28 +0800

    Add sign.py

---
+++ sign.py
@@ -0,0 +1,19 @@
+"""
+    sign.py 符号保留
+    Python 实现 numPy.sign()
+"""
+
+def sign(x):
+    num_sign = []
+    for i in x:
+        if i < 0:
+            num_sign.append(-1)
+        elif i > 0:
+            num_sign.append(1)
+        else:
+            num_sign.append(0)
+    return num_sign
+
+if __name__ == "__main__":
+    x = [9,16,-28,0,1,-2]
+    print(sign(x))
