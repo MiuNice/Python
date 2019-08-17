ed962a592b37e1d702dd086f6b427db6bb5fef02
wMiu <50909599+wMiu@users.noreply.github.com>
2019-08-17 15:35:19 +0800

    Update copy_file.py

--- copy_file.py
+++ copy_file.py
@@ -1,4 +1,11 @@
+"""
+    copy_file.py    拷贝文件
+"""
 def copy_file(file_path, new_file_name):
+    """
+        file_path : 拷贝文件路径
+        new_file_name : 保存文件名称
+    """
     with open(file_path, "rb") as f:
         new_file_path = "./" + new_file_name
         with open(new_file_path, "wb") as nf:
