2df60fd6e25b3b52cdddea8d0d034b8a43337f87
wMiu <50909599+wMiu@users.noreply.github.com>
2019-08-17 18:30:46 +0800

    Update copy_file.py

--- copy_file.py
+++ copy_file.py
@@ -1,13 +1,22 @@
 """
     copy_file.py    拷贝文件
 """
-def copy_file(file_path, new_file_name):
+def copy_file(file_path, new_file_name=None, new_file_path="./"):
     """
-        file_path : 拷贝文件路径
-        new_file_name : 保存文件名称
+        拷贝文件
+    :param file_path: 要拷贝文件的路径
+    :param new_file_name: 保存文件的名称
+    :param new_file_path: 保存文件的路径
+    :return: 
     """
     with open(file_path, "rb") as f:
-        new_file_path = "./" + new_file_name
+        if not new_file_name:
+            new_file_name = file_path.split("/")[-1]
+        if new_file_path.split("/")[-1] == new_file_name:
+            new_file_path = new_file_path
+        else:
+            new_file_path += new_file_name
+        print(new_file_path)
         with open(new_file_path, "wb") as nf:
             while True:
                 data = f.read(1024)
