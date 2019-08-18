5e231d9e65b9b311d0635c723f8af49e182f2df4
wMiu <50909599+wMiu@users.noreply.github.com>
2019-08-18 11:31:24 +0800

    Update copy_file.py v0.11

--- copy_file.py
+++ copy_file.py
@@ -3,25 +3,39 @@
 """
 
 
+class MyFileNotFoundError(Exception):
+    def __init__(self,file_path):
+        super().__init__(self)
+        self._file_path = file_path
+    def __str__(self):
+        return "找不到该路径: " + self._file_path
+
 def copy_file(file_path, new_file_name=None, new_file_path="./"):
     """
         拷贝文件
     :param file_path: 要拷贝文件的路径
     :param new_file_name: 保存文件的名称
     :param new_file_path: 保存文件的路径
-    :return: 
+    :return:
     """
-    with open(file_path, "rb") as f:
-        if not new_file_name:
-            new_file_name = file_path.split("/")[-1]
-        if new_file_path.split("/")[-1] == new_file_name:
-            new_file_path = new_file_path
-        else:
-            new_file_path += new_file_name
-        print(new_file_path)
-        with open(new_file_path, "wb") as nf:
-            while True:
-                data = f.read(1024)
-                if not data:
-                    break
-                nf.write(data)
+    try:
+        with open(file_path, "rb") as f:
+            if not new_file_name:
+                new_file_name = file_path.split("/")[-1]
+            if new_file_path.split("/")[-1] == new_file_name:
+                new_file_path = new_file_path
+            else:
+                if new_file_path[-1] != "/":
+                    new_file_path = new_file_path + "/" + new_file_name
+                else:          
+                    new_file_path += new_file_name
+            print("拷贝地址:", new_file_path)
+            with open(new_file_path, "wb") as nf:
+                while True:
+                    data = f.read(1024)
+                    if not data:
+                        break
+                    nf.write(data)
+    except FileNotFoundError as notfile:
+        error = "".join(list(str(notfile))[38:-1])
+        raise MyFileNotFoundError(error)
