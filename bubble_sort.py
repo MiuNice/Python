c2d6737ec56f6d94ccfe4f72bf44085d6209fda4
wMiu <50909599+wMiu@users.noreply.github.com>
2019-08-18 20:44:12 +0800

    Add bubble_sort.py V0.1

--- bubble_sort.py
+++ bubble_sort.py
@@ -13,13 +13,19 @@ def bubble_sort(_list, reverse=False):
     """
     _len = len(_list)
     for i in range(_len - 1):
+        count = 0
         for j in range(_len - i - 1):
             if reverse:
                 if _list[j] < _list[j + 1]:
                     _list[j], _list[j + 1] = _list[j + 1], _list[j]
+                    count += 1
             else:
                 if _list[j] > _list[j + 1]:
                     _list[j], _list[j + 1] = _list[j + 1], _list[j]
+                    count += 1
+        if count == 0:
+            return _list  # 最优情况 / _len() - 1 为有序列表
+
     return _list
 
 
@@ -27,3 +33,4 @@ if __name__ == "__main__":
     _list = [11, 4, 78, 5, 9, 20, 1, 69, 99]
     list_ = bubble_sort(_list)
     print(list_)
+    
