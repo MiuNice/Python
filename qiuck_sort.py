a660778c5123c2b166d5d4b4b6fd35e43b015d0b
wMiu <50909599+wMiu@users.noreply.github.com>
2019-09-02 16:34:38 +0800

    Update qiuck_sort.py

--- qiuck_sort.py
+++ qiuck_sort.py
@@ -1,20 +1,18 @@
-"""
-    qiuck_sort.py   快速排序
-"""
-def qiuck_sort(_list):
+def quick_sort(_list):
     if len(_list) <= 1:
         return _list
     _left = []
     _right = []
     base = _list.pop()
+
     for i in _list:
         if i >= base:
             _right.append(i)
         else:
             _left.append(i)
-    return qiuck_sort(_left) + [base] + qiuck_sort(_right)
+    return quick_sort(_left) + [base] + quick_sort(_right)
 
 
 if __name__ == "__main__":
     _l = [1, 2, 3, 4, 222, 3, 4, 6, 99]
-    print(qiuck_sort(_l))
+    print(quick_sort(_l))
