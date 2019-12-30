42995ce37447eb47ca10062d5ad67dbed7865ebb
wMiu <miu0619@163.com>
2019-12-30 17:16:14 +0800

    增加 希尔排序

---
+++ Sort/ShellSort.py
@@ -0,0 +1,26 @@
+"""
+    ShellSort.py 希尔排序
+"""
+
+def shell_sort(_list):
+    
+    gap = int(len(_list) / 2)
+    for i in range(gap):
+        step_size = gap if gap > 0 else 1
+        for j in range(gap, len(_list) - 1):
+            now = _list[j + 1]
+            for x in range(j - gap + 1, -1, -step_size):
+                if now < _list[x]:
+                    _list[j + 1] = _list[x]
+                else:
+                    x += step_size
+                    break
+            _list[x] = now
+        gap = int(gap / 2)
+    return _list
+
+
+
+if __name__ == "__main__":
+    _list = [2, 30, 7, 9, 11]
+    print(shell_sort(_list))
