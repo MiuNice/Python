2a5e608c8476b15e964cb7528f2e7493969c9720
wMiu <miu0619@163.com>
2019-12-31 11:12:25 +0800

    修改 ShellSort Bug

--- Sort/ShellSort.py
+++ Sort/ShellSort.py
@@ -1,26 +1,21 @@
 """
-    ShellSort.py 希尔排序 - BUG只能对五个或五个以下长度 的列表做排序操作
+    ShellSort.py 希尔排序
 """
 
 def shell_sort(_list):
-    
-    gap = int(len(_list) / 2)
-    for i in range(gap):
-        step_size = gap if gap > 0 else 1
-        for j in range(gap, len(_list) - 1):
-            now = _list[j + 1]
-            for x in range(j - gap + 1, -1, -step_size):
-                if now < _list[x]:
-                    _list[j + 1] = _list[x]
-                else:
-                    x += step_size
-                    break
-            _list[x] = now
-        gap = int(gap / 2)
+    gap = len(_list) // 2
+    while gap > 0:
+        for i in range(gap, len(_list)):
+            now = _list[i]
+            j = i
+            while j >= gap and now < _list[j - gap]:
+                _list[j] = _list[j - gap]
+                j -= gap
+            _list[j] = now
+        gap = gap // 2
     return _list
 
 
-
 if __name__ == "__main__":
-    _list = [2, 30, 7, 9, 11, 10, 12]
+    _list = [11, 4, 78, 5, 9, 20, 1, 69, 99]
     print(shell_sort(_list))
