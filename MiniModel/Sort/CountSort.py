7b41083572b9830cc874047ffa462a754ab2dab7
wMiu <miu0619@163.com>
2020-01-06 09:58:23 +0800

    增加代码思想-微调代码格式

--- MiniModel/Sort/CountSort.py
+++ MiniModel/Sort/CountSort.py
@@ -1,5 +1,9 @@
 """
     CountSort.py 计数排序
+    思想 : 找到最大值，建立一个长度为最大值的列表值都为0；
+    将 index 抽象为元素的值， value 抽象为元素的个数。
+    [2, 3, 1, 1, 2] -> [0, 2, 2, 1]
+    将抽象后的列表 输出 -> [1, 1, 2, 2, 3]
 """
 
 def count_sort(_list):
@@ -10,7 +14,7 @@ def count_sort(_list):
     sort_list = [0 for x in range(_max + 1)]
     for i in _list:
         sort_list[i] += 1
-    return [ sort for sort in range(len(sort_list)) for j in range(sort_list[sort])]
+    return [sort for sort in range(len(sort_list)) for j in range(sort_list[sort])]
 
 
 if __name__ == "__main__":
