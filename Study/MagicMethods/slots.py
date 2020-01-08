388eff859650003a32f54344c0780e5d71bbfd8a
wMiu <miu0619@163.com>
2020-01-08 14:43:28 +0800

    修改 slots

--- Study/MagicMethods/slots.py
+++ Study/MagicMethods/slots.py
@@ -5,13 +5,15 @@
 
 class Test:
     __slots__ = ("_name", "age")  # 只能绑定 tuple 中的属性(Attribute)
+    # __abc__: int
+    __abc__ = 1
 
     def __init__(self, name):
         self._name = name
 
 
 class ChildTest(Test):
-    # __slots__ = ("age",)  # 子类别需要单独设置 __slots__
+    # __slots__ = ("age",)  # 子类需要单独设置 __slots__
     pass
 
 
@@ -21,9 +23,10 @@ if __name__ == '__main__':
     # Error : AttributeError: 'Test' object has no attribute 'score'
     t.age = 18
     print(t.age)
+    print(t.__abc__)
 
     print("- 分割 -")
 
     c = ChildTest("wMiu")
-    c.score = 99  # 继承后的子类别 不受父类别 __slots__ 限制
+    c.score = 99  # 继承后的子类 不受父类 __slots__ 限制
     print(c.score)
